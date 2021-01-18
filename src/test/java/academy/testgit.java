551ed71f40e2df08d0f5181685845aaa1c56acbf
takrahul22 <sushbhat11@gmail.com>
2021-01-18 23:34:20 +0530

    Changes made by Rahul Tak commited

--- src/test/java/academy/testgit.java
+++ src/test/java/academy/testgit.java
@@ -5,6 +5,9 @@ public class testgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+System.out.println("hello1");
+System.out.println("hello2");
+System.out.println("hello3");
 	}
 
 }
